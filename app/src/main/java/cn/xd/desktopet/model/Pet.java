39723ced5df607a3fc1d961f46007f5a4cd457d9
szx1234 <songzhixin@SongZhinxinDeMacBook-Pro.local>
2017-04-20 10:05:44 +0800

    宠物实体类完善

--- app/src/main/java/cn/xd/desktopet/model/Pet.java
+++ app/src/main/java/cn/xd/desktopet/model/Pet.java
@@ -3,7 +3,10 @@ package cn.xd.desktopet.model;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Random;
+
 import cn.xd.desktopet.R;
+import cn.xd.desktopet.util.Const;
 import cn.xd.desktopet.util.MyApplication;
 
 /**
@@ -18,7 +21,7 @@ public class Pet {
     /**
      * 使用整形常量表示不同的主题
      */
-    public static final int themeDefault=0;
+    public static final int themeDefault = 0;
     /**
      * 姓名
      */
@@ -36,7 +39,7 @@ public class Pet {
      */
     public static String petPhrase;
 
-    static{
+    static {
         SharedPreferences sp = MyApplication.getContext().getSharedPreferences("pet", 0);
         name = sp.getString("name", "UnKnow");
         sex = sp.getString("sex", "Boy");
@@ -47,29 +50,95 @@ public class Pet {
     /**
      * 获取静态图片资源
      */
-    public static int getStillImageId(){
-       return R.drawable.beaver;
+    public static int getStillImageId() {
+        int resId = -1;
+        switch (theme) {
+            case Const.THEME0:
+                resId = R.drawable.beaver;
+                break;
+            case Const.THEME1:
+                resId = R.drawable.beaver;
+                break;
+            case Const.THEME2:
+                resId = R.drawable.beaver;
+                break;
+            default:
+                resId = R.drawable.beaver;
+        }
+        return resId;
     }
 
     /**
      * 获取动态Gif资源
      */
 
-    public static int getMoveImageId(){
-        return R.drawable.dance;
+    public static int getMoveImageId() {
+        int randFaceId = getRandFaceId();
+        int resId = -1;
+        switch (theme){
+            case Const.THEME0:
+                switch (randFaceId){
+                    case Const.HAPPY:
+                       resId = R.drawable.dance;
+                        break;
+                    case Const.SAD:
+                        resId = R.drawable.dance;
+                        break;
+                }
+                break;
+
+            case Const.THEME1:
+                switch (randFaceId){
+                    case Const.HAPPY:
+                        resId = R.drawable.dance;
+                        break;
+                    case Const.SAD:
+                        resId = R.drawable.dance;
+                        break;
+                }
+                break;
+
+            case Const.THEME2:
+                switch (randFaceId){
+                    case Const.HAPPY:
+                        resId = R.drawable.dance;
+                        break;
+                    case Const.SAD:
+                        resId = R.drawable.dance;
+                        break;
+                }
+                break;
+
+            default:
+                resId = R.drawable.dance;
+                break;
+
+        }
+        return resId;
+    }
+
+    /**
+     * 返回随机表情
+     * @return
+     */
+    private static int getRandFaceId() {
+        Random random = new Random();
+        int rand = random.nextInt(Const.FACE_COUNT);
+        return rand;
     }
 
     /**
      * 将宠物当前的设置信息保存到preference文件中
+     *
      * @param context
      */
-    public static void save(Context context){
-        SharedPreferences sp=context.getSharedPreferences("pet",0);
-        SharedPreferences.Editor editor=sp.edit();
-        editor.putString("name",name);
-        editor.putString("sex",sex);
-        editor.putInt("theme",theme);
-        editor.putString("petPhrase",petPhrase);
+    public static void save(Context context) {
+        SharedPreferences sp = context.getSharedPreferences("pet", 0);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("name", name);
+        editor.putString("sex", sex);
+        editor.putInt("theme", theme);
+        editor.putString("petPhrase", petPhrase);
         editor.commit();
     }
 
@@ -77,6 +146,6 @@ public class Pet {
     /**
      * 宠物设置改变时改变Pet中的参数
      */
-    public static void onPetStatusChanged(){
+    public static void onPetStatusChanged() {
     }
 }
--- app/src/main/java/cn/xd/desktopet/util/Const.java
+++ app/src/main/java/cn/xd/desktopet/util/Const.java
@@ -5,5 +5,23 @@ package cn.xd.desktopet.util;
  */
 
 public class Const {
+    /**
+     * 1min
+     */
     public static final int SECOND = 1000;
+
+    /**
+     * 三种主题
+     */
+    public static final int THEME0 = 0;
+    public static final int THEME1 = 1;
+    public static final int THEME2 = 2;
+    public static final int THEME_CONUNT = 3;
+
+    /**
+     * 两种表情
+     */
+    public static final int HAPPY = 0;
+    public static final int SAD = 1;
+    public static final int FACE_COUNT = 2;
 }
